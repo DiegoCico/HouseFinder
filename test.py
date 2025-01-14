fd704c41b404b56bb40690a9c78d09e859abc567
Diego Cicotoste <cicotosted@gmail.com>
2025-01-13 20:37:41 -0500

    need to find a new appproach

---
+++ test.py
@@ -0,0 +1,9 @@
+import pyzill
+import json
+property_url="https://www.zillow.com/apartments/kissimmee-fl/the-nexus-at-overbrook/9DSWrh/"
+proxy_url = pyzill.parse_proxy("[proxy_ip or proxy_domain]","[proxy_port]","[proxy_username]","[proxy_password]")
+data = pyzill.get_from_deparment_url(property_url,proxy_url)
+jsondata = json.dumps(data)
+f = open("details.json", "w")
+f.write(jsondata)
+f.close()
