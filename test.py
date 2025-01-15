a460241f1e8d9344adfda6b83da7af6ee6e717a6
Diego Cicotoste <cicotosted@gmail.com>
2025-01-15 17:07:34 -0500

    no work

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import pyzill
 import json
 property_url="https://www.zillow.com/apartments/kissimmee-fl/the-nexus-at-overbrook/9DSWrh/"
-proxy_url = pyzill.parse_proxy("[proxy_ip or proxy_domain]","[proxy_port]","[proxy_username]","[proxy_password]")
+proxy_url = pyzill.parse_proxy("47.251.122.81","8888","None","None")
 data = pyzill.get_from_deparment_url(property_url,proxy_url)
 jsondata = json.dumps(data)
 f = open("details.json", "w")
